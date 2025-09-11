366a335bc14ba344000eddec62ec40c08ddd5aa2
axhelnq <axhelnq@gmail.com>
2025-09-11 16:04:43 +0300

    chore: setup SCSS & Prettier, add project styles and fix layout bugs

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Усі</li>
+        <li>М'ясні</li>
+        <li>Вегетаріанські</li>
+        <li>Гриль</li>
+        <li>Гострі</li>
+        <li>Закриті</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Categories
